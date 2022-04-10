91504e2d7e266d7c0c8bc5712d7775ec9853c53b
Real-Musafir <alshahadath@gmail.com>
2022-04-10 23:29:32 +0600

    truffle init

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.13", // Fetch exact version from solc-bin (default: truffle's version)
+    },
+  },
+};
