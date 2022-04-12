c16eb1190e25507f2b5b6690137f18687f5ec201
Real-Musafir <alshahadath@gmail.com>
2022-04-12 20:05:01 +0600

    create addFunds function in faucet contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,6 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint public funds = 1000;
+    
+    //this is a special function
+    //it's called when you make a tx that doesn't specify
+    // function name to call
+    receive() external payable {}
+
+    function addFunds() external payable {}
 }
+
+//Block info
+//Nonce -a hash that when combined with the minHash proofs that
+// the  block has gone through of work(POW)
