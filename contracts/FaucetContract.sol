e82f2a25b9bcfbed6158bf971ee0abc6f702782f
Real-Musafir <alshahadath@gmail.com>
2022-04-14 00:08:16 +0600

    make funders array to store funder's address

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,13 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+
+    address[] public funders;
     
     //this is a special function
     //it's called when you make a tx that doesn't specify
     // function name to call
     receive() external payable {}
 
-    function addFunds() external payable {}
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+    
 }
 
 //Block info
