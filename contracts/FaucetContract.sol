101d083a55b130cc30b52ab276aa9a134053660a
Real-Musafir <alshahadath@gmail.com>
2022-04-16 01:04:30 +0600

    manage duplicate fundinge address storage in funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
 
     uint public numOfFunders;
-    mapping(uint=> address) private funders;
+    mapping(address=> bool) private funders;
+    mapping(uint=> address) private lutFunders;
     
     //this is a special function
     //it's called when you make a tx that doesn't specify
@@ -12,22 +13,29 @@ contract Faucet {
     receive() external payable {}
 
     function addFunds() external payable {
-        uint index = numOfFunders++;
-       funders[index] = msg.sender;
+       
+        address funder = msg.sender;
+
+        if(!funders[funder]){
+            uint index = numOfFunders++;
+            funders[funder] = true;
+            lutFunders[index]=funder;
+            
+        }
     }
   
     function getAllFunders() external view returns (address[] memory) {
         address[] memory _funders = new address[](numOfFunders); // Array size created by numOfFunders
 
         for(uint i = 0; i<numOfFunders; i++){
-            _funders[i] = funders[i];
+            _funders[i] = lutFunders[i];
         }
         return _funders;
     }
 
     function getFunderAtIndex(uint8 index) external view returns(address) {
         
-        return funders[index];
+        return lutFunders[index];
     }
     
 }
