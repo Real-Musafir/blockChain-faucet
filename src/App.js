2f5a9fc77c09302abff0ca758b50da7654748786
Real-Musafir <alshahadath@gmail.com>
2022-04-17 22:33:53 +0600

    add bumlaCss and ready home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import "./App.css";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2 ">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
