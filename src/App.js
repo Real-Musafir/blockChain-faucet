d7bc290edb722058034cf2cada14e240b84f6991
Real-Musafir <alshahadath@gmail.com>
2022-04-17 23:05:50 +0600

    requeset meatamask with eth_requestAccounts methods

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const loadProvider = async () => {
+      //with metamas we hava an access to window.ethereum & to window.web3
+      // metamask injecxts a global API into website
+      // this API allows websites to request user, ,account, read data to blockchain,
+      // sign messages and transactions
+
+      console.log(window.web3);
+      console.log(window.ethereum);
+    };
+    loadProvider();
+  }, []);
   return (
     <>
       <div className="faucet-wrapper">
@@ -9,6 +21,17 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong> ETH
           </div>
+          <button
+            onClick={async () => {
+              const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+              });
+              console.log(accounts);
+            }}
+            className="btn mr-2"
+          >
+            Enable Ethereum
+          </button>
           <button className="btn mr-2 ">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
