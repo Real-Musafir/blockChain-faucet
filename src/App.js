8c43da7aff066d90ed3f1980c92b62a98c36560a
Real-Musafir <alshahadath@gmail.com>
2022-04-18 19:33:07 +0600

    create connect button to connect with metamask

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     const loadProvider = async () => {
       let provider = await detectEthereumProvider();
       if (provider) {
-        provider.request({ method: "eth_requestAccounts" });
+        // provider.request({ method: "eth_requestAccounts" });
         setWeb3Api({
           web3: new Web3(provider),
           provider,
@@ -39,16 +39,30 @@ function App() {
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <span>
-            <strong>Account:</strong>
-          </span>
-          <h1>{account ? account : "not connected"}</h1>
-          <div className="balance-view is-size-2">
+          <div className="is-flex is-align-items-center">
+            <strong className="mr-2">
+              <strong>Account:</strong>
+            </strong>
+
+            {account ? (
+              <span> {account}</span>
+            ) : (
+              <button
+                onClick={() =>
+                  web3Api.provider.request({ method: "eth_requestAccounts" })
+                }
+                className="button  is-small mr-2"
+              >
+                Connect Wallet
+              </button>
+            )}
+          </div>
+          <div className="balance-view is-size-2 my-5">
             Current Balance: <strong>10</strong> ETH
           </div>
 
-          <button className="btn mr-2 ">Donate</button>
-          <button className="btn">Withdraw</button>
+          <button className="button  is-link mr-2 ">Donate</button>
+          <button className="button is-primary">Withdraw</button>
         </div>
       </div>
     </>
